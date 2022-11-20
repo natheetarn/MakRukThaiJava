a237cf1b76ce94d1d175f154598ecb196a5689b9
Sorracha-A <64011628@kmitl.ac.th>
2022-11-20 15:38:28 +0700

    original board

--- src/thaichessui/Board.java
+++ src/thaichessui/Board.java
@@ -65,80 +65,80 @@ public class Board {
 
     void setStartingPiecesWhite(boolean isHostView) {
         if (isHostView) {
-            // for (int i = 0; i < 8; i++) {
-            // board[5][i].setPiece(new BiaPiece(Color.WHITE));
-            // }
+            for (int i = 0; i < 8; i++) {
+            board[5][i].setPiece(new BiaPiece(Color.WHITE));
+            }
 
             board[7][0].setPiece(new RuaPiece(Color.WHITE));
-            // board[7][7].setPiece(new RuaPiece(Color.WHITE));
+            board[7][7].setPiece(new RuaPiece(Color.WHITE));
 
-            // board[7][1].setPiece(new MahPiece(Color.WHITE));
-            // board[7][6].setPiece(new MahPiece(Color.WHITE));
+            board[7][1].setPiece(new MahPiece(Color.WHITE));
+            board[7][6].setPiece(new MahPiece(Color.WHITE));
 
-            // board[7][2].setPiece(new ConePiece(Color.WHITE));
-            // board[7][5].setPiece(new ConePiece(Color.WHITE));
+            board[7][2].setPiece(new ConePiece(Color.WHITE));
+            board[7][5].setPiece(new ConePiece(Color.WHITE));
 
             board[7][3].setPiece(new KhunPiece(Color.WHITE));
-            // board[7][4].setPiece(new MetPiece(Color.WHITE));
+            board[7][4].setPiece(new MetPiece(Color.WHITE));
 
         } else {
-            // for (int i = 0; i < 8; i++) {
-            // board[getOpposite(5)][getOpposite(i)].setPiece(new BiaPiece(Color.WHITE));
-            // }
+            for (int i = 0; i < 8; i++) {
+            board[getOpposite(5)][getOpposite(i)].setPiece(new BiaPiece(Color.WHITE));
+            }
 
             // // rook
             board[getOpposite(7)][getOpposite(0)].setPiece(new RuaPiece(Color.WHITE));
-            // board[getOpposite(7)][getOpposite(7)].setPiece(new RuaPiece(Color.WHITE));
+            board[getOpposite(7)][getOpposite(7)].setPiece(new RuaPiece(Color.WHITE));
 
-            // // ่knight
-            // board[getOpposite(7)][getOpposite(1)].setPiece(new MahPiece(Color.WHITE));
-            // board[getOpposite(7)][getOpposite(6)].setPiece(new MahPiece(Color.WHITE));
+            // ่knight
+            board[getOpposite(7)][getOpposite(1)].setPiece(new MahPiece(Color.WHITE));
+            board[getOpposite(7)][getOpposite(6)].setPiece(new MahPiece(Color.WHITE));
 
-            // // cone
-            // board[getOpposite(7)][getOpposite(2)].setPiece(new ConePiece(Color.WHITE));
-            // board[getOpposite(7)][getOpposite(5)].setPiece(new ConePiece(Color.WHITE));
+            // cone
+            board[getOpposite(7)][getOpposite(2)].setPiece(new ConePiece(Color.WHITE));
+            board[getOpposite(7)][getOpposite(5)].setPiece(new ConePiece(Color.WHITE));
 
             board[getOpposite(7)][getOpposite(3)].setPiece(new KhunPiece(Color.WHITE));
-            // board[getOpposite(7)][getOpposite(4)].setPiece(new MetPiece(Color.WHITE));
+            board[getOpposite(7)][getOpposite(4)].setPiece(new MetPiece(Color.WHITE));
         }
     }
 
     void setStartingPiecesBlack(boolean isHostView) {
         if (isHostView) {
-            // for (int i = 0; i < 8; i++) {
-            // board[getOpposite(5)][getOpposite(i)].setPiece(new BiaPiece(Color.BLACK));
-            // }
+            for (int i = 0; i < 8; i++) {
+            board[getOpposite(5)][getOpposite(i)].setPiece(new BiaPiece(Color.BLACK));
+            }
 
             board[getOpposite(7)][getOpposite(0)].setPiece(new RuaPiece(Color.BLACK));
             board[getOpposite(7)][getOpposite(7)].setPiece(new RuaPiece(Color.BLACK));
 
             // ่knight
-            board[getOpposite(7)][getOpposite(1)].setPiece(new RuaPiece(Color.BLACK));
-            board[getOpposite(7)][getOpposite(6)].setPiece(new RuaPiece(Color.BLACK));
+            board[getOpposite(7)][getOpposite(1)].setPiece(new MahPiece(Color.BLACK));
+            board[getOpposite(7)][getOpposite(6)].setPiece(new MahPiece(Color.BLACK));
 
             // cone
-            board[getOpposite(7)][getOpposite(2)].setPiece(new RuaPiece(Color.BLACK));
-            board[getOpposite(7)][getOpposite(5)].setPiece(new RuaPiece(Color.BLACK));
+            board[getOpposite(7)][getOpposite(2)].setPiece(new ConePiece(Color.BLACK));
+            board[getOpposite(7)][getOpposite(5)].setPiece(new ConePiece(Color.BLACK));
 
             board[getOpposite(7)][getOpposite(3)].setPiece(new KhunPiece(Color.BLACK));
-            board[getOpposite(7)][getOpposite(4)].setPiece(new RuaPiece(Color.BLACK));
+            board[getOpposite(7)][getOpposite(4)].setPiece(new MetPiece(Color.BLACK));
         } else {
-            // for (int i = 0; i < 8; i++) {
-            // board[5][i].setPiece(new BiaPiece(Color.BLACK));
+            for (int i = 0; i < 8; i++) {
+            board[5][i].setPiece(new BiaPiece(Color.BLACK));
 
-            // }
+            }
 
             board[7][0].setPiece(new RuaPiece(Color.BLACK));
             board[7][7].setPiece(new RuaPiece(Color.BLACK));
 
-            board[7][1].setPiece(new RuaPiece(Color.BLACK));
-            board[7][6].setPiece(new RuaPiece(Color.BLACK));
+            board[7][1].setPiece(new MahPiece(Color.BLACK));
+            board[7][6].setPiece(new MahPiece(Color.BLACK));
 
-            board[7][2].setPiece(new RuaPiece(Color.BLACK));
-            board[7][5].setPiece(new RuaPiece(Color.BLACK));
+            board[7][2].setPiece(new ConePiece(Color.BLACK));
+            board[7][5].setPiece(new ConePiece(Color.BLACK));
 
             board[7][3].setPiece(new KhunPiece(Color.BLACK));
-            board[7][4].setPiece(new RuaPiece(Color.BLACK));
+            board[7][4].setPiece(new MetPiece(Color.BLACK));
         }
     }
 
