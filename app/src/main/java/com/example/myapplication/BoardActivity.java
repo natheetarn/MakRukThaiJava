1f425d679a1d39ba20bf46d4be37835ae11df39d
Phobphoomin Siriboon <90683904+prompto416@users.noreply.github.com>
2022-11-22 05:09:31 +0700

    Added several tiles for testing in BoardActivity

--- app/src/main/java/com/example/myapplication/BoardActivity.java
+++ app/src/main/java/com/example/myapplication/BoardActivity.java
@@ -7,6 +7,7 @@ import androidx.core.content.ContextCompat;
 import android.annotation.SuppressLint;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -27,32 +28,13 @@ public class BoardActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_board);
 
-        initBoard();
-    }
-    @SuppressLint("SetTextI18n")
-    protected void initBoard(){
-        RelativeLayout layout_tiles = (RelativeLayout) findViewById(R.id.layout_board);
-
-        ArrayList<ImageButton> tiles = new ArrayList<>();
-        //MaterialButton button = new MaterialButton(getBaseContext());
-        MaterialButton button = new MaterialButton(this);
-        MaterialButton button2 = new MaterialButton(this);
-
-        button.setBackgroundColor(Color.BLACK);
-        button.setText("How old are you2");
-        button.setId(0);
-
-        button.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                button2.setText("clicked");
-                //button2.setIcon(ContextCompat.getDrawable(getBaseContext(),R.drawable.bia_white));
-            }
-        });
-
-        layout_tiles.addView(button);
-//        layout_tiles.addView(button2);
-
 
     }
+
+
+
+
+
+
+
 }
