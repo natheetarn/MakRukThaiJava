5afb6c16834740ed33189cbbcb62a7d4aafc687a
Phobphoomin Siriboon <90683904+prompto416@users.noreply.github.com>
2022-11-23 03:11:49 +0700

    undo change since either way you have to specify import

---
+++ app/src/main/java/com/example/myapplication/BoardComponents/Tile.java
@@ -0,0 +1,7 @@
+package com.example.myapplication.BoardComponents;
+
+import com.example.myapplication.Pieces.Piece;
+
+public class Tile {
+    private Piece piece;
+}
--- app/src/main/java/com/example/myapplication/Tile.java
+++
@@ -1,5 +0,0 @@
-package com.example.myapplication;
-
-public class Tile {
-    
-}
