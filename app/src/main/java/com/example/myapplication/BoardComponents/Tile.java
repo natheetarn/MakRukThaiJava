7e1118445faf5023b07b7cc75910efd74720a755
Phobphoomin Siriboon <90683904+prompto416@users.noreply.github.com>
2022-11-23 03:09:45 +0700

    Turns out components dir was a bad idea

--- app/src/main/java/com/example/myapplication/BoardComponents/Tile.java
+++
@@ -1,9 +0,0 @@
-package com.example.myapplication.BoardComponents;
-
-import com.example.myapplication.Pieces.Piece;
-
-public class Tile {
-    private boolean isOccupied = false;
-    private Piece piece;
-
-}
