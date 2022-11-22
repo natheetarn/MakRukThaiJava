16b52c68ef9c5a3643557336c4912a25bc3e7cfb
Phobphoomin Siriboon <90683904+prompto416@users.noreply.github.com>
2022-11-22 17:08:52 +0700

    converted constraint to relativelayout on BoardActivity

--- app/src/main/java/com/example/myapplication/BoardActivity.java
+++ app/src/main/java/com/example/myapplication/BoardActivity.java
@@ -6,8 +6,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -29,27 +31,35 @@ public class BoardActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_board);
 
-        MaterialButton btn_a8 = (MaterialButton) findViewById(R.id.button_a8);
+        RelativeLayout  layout_board = (RelativeLayout) findViewById(R.id.layout_board);
 
-        btn_a8.setOnClickListener(new View.OnClickListener(){
+        Button btn1 = new Button(this);
+        float btnSize_float = convertDpToPixel(60,this);
+        int btnSize_int = (int)Math.round(btnSize_float);
+        btn1.setLayoutParams(new RelativeLayout.LayoutParams(btnSize_int,btnSize_int));
+        btn1.setCompoundDrawablesWithIntrinsicBounds(0,0,0,R.drawable.bia_white);
+        btn1.setBackgroundColor(getResources().getColor(R.color.warm_green));
+        btn1.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                a8_clicked();
+                btn1.setCompoundDrawablesWithIntrinsicBounds(0,0,0,0);
+                btn1.setText("clicked");
             }
         });
+        layout_board.addView(btn1);
 
 
-    }
-
-    public void a8_clicked(){
-        MaterialButton btn_a7 = (MaterialButton) findViewById(R.id.button_a7);
-
-        Log.d("logtag",getResources().getResourceEntryName(R.id.button_a7));
-        ;
-
-        btn_a7.setCompoundDrawablesWithIntrinsicBounds(0,0,0,R.drawable.bia_white);
 
     }
+
+    public static float convertDpToPixel(float dp, Context context){
+        return dp * ((float) context.getResources().getDisplayMetrics().densityDpi / DisplayMetrics.DENSITY_DEFAULT);
+    }
+
+    public static float convertPixelsToDp(float px, Context context){
+        return px / ((float) context.getResources().getDisplayMetrics().densityDpi / DisplayMetrics.DENSITY_DEFAULT);
+    }
+
 
 
 
