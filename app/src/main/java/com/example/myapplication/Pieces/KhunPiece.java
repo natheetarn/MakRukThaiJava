1d12c9642b712e8b9afdcf20475262b0f3999c6d
Phobphoomin Siriboon <90683904+prompto416@users.noreply.github.com>
2022-11-23 02:42:42 +0700

    added default constructor for KhunPiece

--- app/src/main/java/com/example/myapplication/Pieces/KhunPiece.java
+++ app/src/main/java/com/example/myapplication/Pieces/KhunPiece.java
@@ -1,5 +1,10 @@
 package com.example.myapplication.Pieces;
 
-public class KhunPiece {
+public class KhunPiece extends Piece{
 
+
+    public KhunPiece(boolean isWhite) {
+        super(isWhite);
+        this.name = "KhunPiece";
+    }
 }
