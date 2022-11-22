cf006f2e0b90fcf9d31133849bd1fee3172a849e
Phobphoomin Siriboon <90683904+prompto416@users.noreply.github.com>
2022-11-23 03:08:32 +0700

    Specified directory to import class

--- app/src/main/java/com/example/myapplication/BoardComponents/Tile.java
+++ app/src/main/java/com/example/myapplication/BoardComponents/Tile.java
@@ -1,5 +1,9 @@
 package com.example.myapplication.BoardComponents;
 
+import com.example.myapplication.Pieces.Piece;
+
 public class Tile {
-    
+    private boolean isOccupied = false;
+    private Piece piece;
+
 }
