b047b239ee09ff4cbd7c732c1b5b5394aabfd3c5
Phobphoomin Siriboon <90683904+prompto416@users.noreply.github.com>
2022-11-23 02:51:52 +0700

    added PromotedBiaPiece which extends from Met ty Natheetarnx for algo

--- app/src/main/java/com/example/myapplication/Pieces/MahPiece.java
+++ app/src/main/java/com/example/myapplication/Pieces/MahPiece.java
@@ -1,4 +1,10 @@
 package com.example.myapplication.Pieces;
 
-public class MahPiece {
+public class MahPiece extends Piece{
+
+
+    public MahPiece(boolean isWhite) {
+        super(isWhite);
+        this.name = "MahPiece";
+    }
 }
---
+++ app/src/main/java/com/example/myapplication/Pieces/PromotedBiaPiece.java
@@ -0,0 +1,10 @@
+package com.example.myapplication.Pieces;
+
+public class PromotedBiaPiece extends MetPiece{
+
+    public PromotedBiaPiece(boolean isWhite) {
+        super(isWhite);
+        this.name = "PromotedBiaPiece";
+
+    }
+}
