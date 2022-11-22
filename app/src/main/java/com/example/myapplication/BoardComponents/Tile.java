ca24776f8403e5a33c7ab30d2d6f93e221f4b923
Phobphoomin Siriboon <90683904+prompto416@users.noreply.github.com>
2022-11-23 03:13:38 +0700

    Added tile method

--- app/src/main/java/com/example/myapplication/BoardComponents/Tile.java
+++ app/src/main/java/com/example/myapplication/BoardComponents/Tile.java
@@ -4,4 +4,36 @@ import com.example.myapplication.Pieces.Piece;
 
 public class Tile {
     private Piece piece;
+    private boolean isOccupied = false;
+    private boolean isSelected = false;
+
+    public boolean getOccupied() {
+        return this.isOccupied;
+    }
+
+    public boolean getSelected() {
+        return this.isSelected;
+    }
+
+    public boolean setSelected(boolean selected) {
+        return this.isSelected = selected;
+    }
+
+    public void setOccupied(boolean o) {
+        this.isOccupied = o;
+    }
+
+    public Piece getPiece() {
+        return this.piece;
+    }
+
+    public void setPiece(Piece p) {
+        this.piece = p;
+        if (p != null) {
+            this.setOccupied(true);
+        } else {
+            this.setOccupied(false);
+        }
+    }
+
 }
