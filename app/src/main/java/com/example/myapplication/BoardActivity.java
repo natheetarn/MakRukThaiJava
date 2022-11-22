f8ca134f2b514525e01b7f0cd8782d31b6f689c3
Phobphoomin Siriboon <90683904+prompto416@users.noreply.github.com>
2022-11-23 01:53:34 +0700

    declared screen size for responsive object size

--- app/src/main/java/com/example/myapplication/BoardActivity.java
+++ app/src/main/java/com/example/myapplication/BoardActivity.java
@@ -1,27 +1,14 @@
 package com.example.myapplication;
 
-import androidx.annotation.ColorInt;
-import androidx.annotation.DrawableRes;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
-
-import android.annotation.SuppressLint;
 import android.content.Context;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
-
-import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
-import com.google.android.material.button.MaterialButton;
-
-import java.sql.Array;
-import java.util.ArrayList;
+import androidx.appcompat.app.AppCompatActivity;
 
 
 public class BoardActivity extends AppCompatActivity {
@@ -32,14 +19,23 @@ public class BoardActivity extends AppCompatActivity {
         setContentView(R.layout.activity_board);
 
         //Global Variable Scope
-        float btnSize_float = convertDpToPixel(60,this);
-        int btnSize_int = (int)Math.round(btnSize_float);
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        int screenHeight = displayMetrics.heightPixels;
+        int screenWidth = displayMetrics.widthPixels;
+
+
+        Log.d("logtag",Float.toString(screenHeight));
+        System.exit(1);
+        //hardcoded button can delete after we have new way to find button size
+//        float btnSize_float = convertDpToPixel(60,this);
+//        int btnSize_int = (int)Math.round(btnSize_float);
         RelativeLayout  layout_board = (RelativeLayout) findViewById(R.id.layout_board);
 
 
         //for base button just create button and specify relative layout width and height then set bottom drawable, background color and set it to randomly generated id
         Button btn1 = new Button(this);
-        btn1.setLayoutParams(new RelativeLayout.LayoutParams(btnSize_int,btnSize_int));
+        btn1.setLayoutParams(new RelativeLayout.LayoutParams(0,0));
         btn1.setCompoundDrawablesWithIntrinsicBounds(0,0,0,R.drawable.bia_white);
         btn1.setBackgroundColor(getResources().getColor(R.color.warm_green));
         btn1.setId(View.generateViewId());
@@ -50,7 +46,7 @@ public class BoardActivity extends AppCompatActivity {
                 btn1.setText("clicked");
 
                 //design a specific layout for btn2
-                RelativeLayout.LayoutParams params2= new RelativeLayout.LayoutParams(btnSize_int,btnSize_int);
+                RelativeLayout.LayoutParams params2= new RelativeLayout.LayoutParams(0,0);
                 params2.addRule(RelativeLayout.BELOW, btn1.getId());
                 //create button 2
                 Button btn2 = new Button(getBaseContext());
